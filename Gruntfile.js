05697e63cbc6f4220ece7751cf1294c56f5b2817
Adrian Cerbaro <adriancerbaro@gmail.com>
2020-03-20 12:59:41 -0300

    Grunt configured with uglify, sass and watch.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,18 +14,45 @@ module.exports = function(grunt) {
             src: "**/*.js",
             dest: "app/js",
             cwd: "src/js",
-            rename: function(dst, src) {
-                return `${dst}/${src.replace(".js", ".min.js")}`
-            }
+            ext: ".min.js"
           }
         ]
       }
+    },
+    sass: {
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: "src/scss",
+            src: ["*.scss"],
+            dest: "app/css",
+            ext: ".css"
+          }
+        ]
+      }
+    },
+    watch: {
+        scripts: {
+            files: 'src/js/**/*.js',
+            tasks: ['uglify'],
+            options: {
+                interrupt: true
+            }
+        },
+        styles: {
+            files: 'src/scss/**/*.scss',
+            tasks: ['sass']
+        }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks("grunt-contrib-uglify-es");
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 
   // Default task(s).
-  grunt.registerTask("default", ["uglify"]);
+  grunt.registerTask("default", ["uglify", "sass"]);
+  grunt.registerTask("dev", ["watch"]);
 };
